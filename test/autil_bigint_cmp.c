163d48d34da484ab4233bb690e8176a577b5207f
ashn <me@ashn.dev>
2020-11-12 17:04:51 -0500

    Run clang-format

--- test/autil_bigint_cmp.c
+++ test/autil_bigint_cmp.c
@@ -7,15 +7,15 @@ main(void)
 {
     struct autil_bigint* LHS = NULL;
     struct autil_bigint* RHS = NULL;
-#define TEST_VALID(lhs_, rhs_, op_) \
-    LHS = autil_bigint_new_cstr(lhs_); \
-    RHS = autil_bigint_new_cstr(rhs_); \
-    ASSERT(LHS != NULL); \
-    ASSERT(RHS != NULL); \
-    EXPECT(autil_bigint_cmp(LHS, RHS) op_ 0); \
-    EXPECT(autil_bigint_vpcmp(LHS, RHS) op_ 0); \
-    autil_bigint_del(LHS); \
-    autil_bigint_del(RHS); \
+#define TEST_VALID(lhs_, rhs_, op_)                                            \
+    LHS = autil_bigint_new_cstr(lhs_);                                         \
+    RHS = autil_bigint_new_cstr(rhs_);                                         \
+    ASSERT(LHS != NULL);                                                       \
+    ASSERT(RHS != NULL);                                                       \
+    EXPECT(autil_bigint_cmp(LHS, RHS) op_ 0);                                  \
+    EXPECT(autil_bigint_vpcmp(LHS, RHS) op_ 0);                                \
+    autil_bigint_del(LHS);                                                     \
+    autil_bigint_del(RHS);
 
     TEST_VALID("0", "0", ==);
     TEST_VALID("+1", "+1", ==);
