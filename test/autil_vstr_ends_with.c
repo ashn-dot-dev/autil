3ad9b2d79e4e7065ae170ac07f3e9495ba7072c2
ashn <me@ashn.dev>
2021-01-22 18:44:56 -0500

    Run clang-format

--- test/autil_vstr_ends_with.c
+++ test/autil_vstr_ends_with.c
@@ -5,38 +5,27 @@
 int
 main(void)
 {
-    EXPECT(
-        autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR("", 0),
-            AUTIL_VSTR_LOCAL_PTR("", 0)));
-    EXPECT(
-        autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR("FOOBAR", 6),
-            AUTIL_VSTR_LOCAL_PTR("", 0)));
-    EXPECT(
-        autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR("FOOBAR", 3),
-            AUTIL_VSTR_LOCAL_PTR("", 0)));
+    EXPECT(autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR("", 0), AUTIL_VSTR_LOCAL_PTR("", 0)));
+    EXPECT(autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR("FOOBAR", 6), AUTIL_VSTR_LOCAL_PTR("", 0)));
+    EXPECT(autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR("FOOBAR", 3), AUTIL_VSTR_LOCAL_PTR("", 0)));
 
-    EXPECT(
-        autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOBAR"),
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("BAR")));
-    EXPECT(
-        !autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOBAR"),
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOO")));
-    EXPECT(
-        !autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOO"),
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOBAR")));
-    EXPECT(
-        !autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOO"),
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOFOO")));
+    EXPECT(autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOBAR"),
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("BAR")));
+    EXPECT(!autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOBAR"),
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOO")));
+    EXPECT(!autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOO"),
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOBAR")));
+    EXPECT(!autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOO"),
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("FOOFOO")));
 
-    EXPECT(
-        autil_vstr_ends_with(
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("\0FOO\0BAR"),
-            AUTIL_VSTR_LOCAL_PTR_LITERAL("\0BAR")));
+    EXPECT(autil_vstr_ends_with(
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("\0FOO\0BAR"),
+        AUTIL_VSTR_LOCAL_PTR_LITERAL("\0BAR")));
 }
