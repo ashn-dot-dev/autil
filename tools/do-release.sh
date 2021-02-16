dd3ba91e3a8e43ba62f02e18fc68571ec31ed6e0
ashn <me@ashn.dev>
2021-02-15 22:34:51 -0500

    Update tools/do-release.sh

--- tools/do-release.sh
+++ tools/do-release.sh
@@ -3,17 +3,19 @@
 set -e
 
 usage() {
-    echo "Usage: $0 VERSION"
+    echo "Usage: $(basename $0) VERSION"
     echo "Prepare a release of autil with the provided version number."
-    echo "VERSION should be in the form MAJOR.MINOR.PATCH."
+    echo "VERSION should be in the form v{MAJOR}.{MINOR}.{PATCH}:"
+    echo "Example:"
+    echo "  \$ sh $(basename $0) v3.14.1"
     printf '\n'
     echo "Full release process:"
     echo "  \$ sh $0 \${VERSION}"
     echo "  \$ git add --all"
-    echo "  \$ git commit --message='Release v\${VERSION}'"
+    echo "  \$ git commit --message='Release \${VERSION}'"
     echo "  \$ git push origin master"
-    echo "  \$ git tag v\${VERSION}"
-    echo "  \$ git push origin v\${VERSION}"
+    echo "  \$ git tag \${VERSION}"
+    echo "  \$ git push origin \${VERSION}"
 }
 
 if [ $# != '1' ]; then
@@ -26,12 +28,12 @@ DATE="$(date +"%Y-%m-%d")" # ISO-8601
 
 for file in *.h; do
     cat "${file}" | \
-        sed "s/ - v[0-9]*\.[0-9]*\.[0-9]*$/ - v${VERSION}/" >"${file}.tmp"
+        sed "s/ - v[0-9]*\.[0-9]*\.[0-9]*$/ - ${VERSION}/" >"${file}.tmp"
     mv "${file}.tmp" "${file}"
 done
 
 for file in CHANGELOG*.txt; do
     cat "${file}" | \
-        sed "s/^Unreleased/v${VERSION} - ${DATE}/" >"${file}.tmp"
+        sed "s/^Unreleased/${VERSION} - ${DATE}/" >"${file}.tmp"
     mv "${file}.tmp" "${file}"
 done
