3d6a84a80cd5543d9bca93c8ecc1868ca262b7de
ashn <me@ashn.dev>
2021-03-14 14:13:02 -0400

    Release v0.9.0

--- atest.h
+++ atest.h
@@ -1,5 +1,5 @@
 /*
-ATEST - ASHN'S TESTING LIBRARY - v0.8.0
+ATEST - ASHN'S TESTING LIBRARY - v0.9.0
     Single header file for unit testing in C99.
     This library has no dependencies outside of libc.
 
--- autil.h
+++ autil.h
@@ -1,5 +1,5 @@
 /*
-AUTIL - ASHN'S UTILITY LIBRARY - v0.8.0
+AUTIL - ASHN'S UTILITY LIBRARY - v0.9.0
     Single header file containing functions and data structures for rapid
     application development in C99 (or later) with a focus on simplicity.
     This library has no dependencies outside of libc.
