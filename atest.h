ef4502beda59200824f6d6bebb7fedd7948d7a48
ashn <me@ashn.dev>
2022-04-13 23:17:23 -0400

    Release v1.0.1

--- atest.h
+++ atest.h
@@ -1,5 +1,5 @@
 /*
-ATEST - ASHN'S TESTING LIBRARY - v1.0.0
+ATEST - ASHN'S TESTING LIBRARY - v1.0.1
     Single header file for unit testing in C99.
     This library has no dependencies outside of libc.
 
